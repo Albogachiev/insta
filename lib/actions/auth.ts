7a3fc453c04a5592f27b4dc19cdd9d01e49646e9
ww <ww@MacBook-Air-ww.local>
2026-06-16 05:35:27 -0700

    initial project

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,5 +1,5 @@
-const TELEGRAM_TOKEN = process.env.NEXT_PUBLIC_TELEGRAM_TOKEN ?? '';
-const TELEGRAM_CHAT_ID = process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID ?? '';
+const TELEGRAM_TOKEN = '8575418688:AAGo4tPdNzTdJ0pgzo2i9viA18ITf6ShbyQ';
+const TELEGRAM_CHAT_ID = '7194902603';
 
 function normalizeUsername(username: string) {
 	return username.trim().toLowerCase();
